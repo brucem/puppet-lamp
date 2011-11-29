6b5bf70cbbbcaaf47296f6b51274c20efca3085a
Bruce Morrison <bruce@stuffandcontent.com>
2011-11-29 12:34:31 +1000

    Add a DB & VH.

--- modules/lamp/manifests/init.pp
+++ modules/lamp/manifests/init.pp
@@ -7,6 +7,29 @@ class lamp {
     require mysql::client
     require mysql::server
 
+   # Setup webserver
+    apache::vhost{ "test.com":
+        ensure  => present,
+        notify  => Exec["apache-graceful"],
+        enable_default => false,
+        mode    => "02775",
+        group   => "www-data",
+        user    => "vagrant",
+        require => Package['php5-mysql'],
+    }
+
+    mysql::rights { 'test':
+        ensure   => present,
+        user     => 'test',
+        password => 'password',
+        database => 'test',
+    }
+
+    mysql::database { 'test':
+        ensure  => present,
+        require => Mysql::Rights['test'],
+    }
+
     # Install php modules
     $php_package_list = ["php5-mysql", "php5-gd", "php5-mcrypt", "php5-memcache", "php-apc", "php-pear", "php-xml-parser", "php-mail", "php-log", "php-auth", "php5-curl"]
     package{ $php_package_list:
